3df4499fc94a0aafb0a701d770fff566ab5690e9
lukas_christopher <lukas.santana@aluno.uepb.edu.br>
2026-02-28 11:35:33 -0300

    grafico praticamente finalizado

--- app/reserva_investimento/Total.js
+++ app/reserva_investimento/Total.js
@@ -1,9 +1,9 @@
-import styles from "@/app/reserva_investimento/page.module.css"
+import styles from "@/app/reserva_investimento/total.module.css"
 
 export default function Total() {
     return(
         <main className={styles.total}>
-            <p className={styles.titulo_total}>Total Guardado Atualmente</p>
+            <p className={styles.titulo}>Total Guardado Atualmente</p>
             <p className={styles.total_guardado}>R$ 5.200,00</p>
         </main>
     )
--- app/reserva_investimento/grafico.js
+++ app/reserva_investimento/grafico.js
@@ -1,15 +1,17 @@
 "use client"
 
 import {
-    LineChart,
-    Line,
+    AreaChart,
+    Area,
     XAxis,
     YAxis,
     Tooltip,
-    ResponsiveContainer
+    ResponsiveContainer,
+    CartesianGrid,
+    Legend
 } from 'recharts'
 
-import styles  from "@/app/reserva_investimento/page.module.css"
+import styles  from "@/app/reserva_investimento/grafico.module.css"
 
 const dados = [
     { data: "2026-01-01", valor: 1000 },
@@ -22,16 +24,33 @@ const dados = [
 
 export default function Grafico() {
     return (
-        <main>
-            <p className={styles.titulo_do_grafico} >Evolução das Reservas</p>
-            <figure className={styles.conteudo_do_grafico}>
+        <main className={styles.container}>
+            <p className={styles.titulo}>Evolução das Reservas</p>
+            <figure className={styles.grafico}>
                 <ResponsiveContainer width="100%" height={300}>
-                    <LineChart data={dados}>
-                        <XAxis dataKey="data" />
+                    <AreaChart data={dados}>
+                        <Legend
+                            iconType="rect" 
+                            verticalAlign="top" 
+                            align="center"
+                        />
+                        <CartesianGrid stroke="#e5e7eb" />
+                        <XAxis 
+                            dataKey="data"
+                            tickMargin={15}
+                        />
                         <YAxis dataKey="valor" />
                         <Tooltip />
-                        <Line type="monotone" dataKey="valor" stroke="#2563eb" />
-                    </LineChart>
+                        <Area 
+                            type="monotone" 
+                            dataKey="valor" 
+                            name="Total Acumulado"
+                            stroke="#16a34a"
+                            fill="#16a34a" 
+                            fillOpacity={0.2}
+                            strokeWidth={2}
+                        />
+                    </AreaChart>
                 </ResponsiveContainer>
             </figure>
         </main>
