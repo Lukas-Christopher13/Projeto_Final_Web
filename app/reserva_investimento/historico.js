48b3c7e61a9c3d0cf8e2ff934d9ef956fb6333c9
lukas_christopher <lukas.santana@aluno.uepb.edu.br>
2026-03-03 20:39:08 -0300

    formulario de edição feito!

--- app/reserva_investimento/historico.js
+++ app/reserva_investimento/historico.js
@@ -3,15 +3,24 @@
 import styles  from "@/app/reserva_investimento/historico.module.css"
 import ExportarHistoricoButton from "./components/ExportarHistoricoButton"
 import { useEffect, useState } from "react"
+import { useForm } from "react-hook-form"
 import { FaPen, FaTrash } from "react-icons/fa"
 import Modal from "./components/Modal"
+import TextInput from "./components/TextInput"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { aporteSchema } from "@/schemas/aporte.schema"
 
 
 export default function Historico() {
+    const { register, handleSubmit, formState: { errors }} = useForm({
+        resolver: zodResolver(aporteSchema),
+        mode: "onBlur",
+    })
+
     const [aports, setAportes] = useState([])
     const [aporteId, setAporteId] = useState(null)
     const [isOpen, setIsOpen] = useState(false)
-
+    const [isEditModalOpen, setEditModal] = useState(false)
    
     function abrirModal(id) {
         setAporteId(id)
@@ -55,11 +64,50 @@ export default function Historico() {
             <Modal isOpen={isOpen} onClose={() => fecharModal()} >
                 <div className={styles.excluir}>
                     <p>Realmente deseja excluir o aporte?</p>
-                    <div className={styles.botoes_excluir}>
+                    <div className={styles.botoes}>
                         <button className={styles.botao_cancelar} onClick={() => fecharModal()} >Cancelar</button>
                         <button className={styles.botao_excluir} onClick={() => excluirAporte()} >Excluir</button>
                     </div>
                 </div>
+            </Modal>
+
+            <Modal isOpen={isEditModalOpen} onClose={() => setEditModal(false)}>
+                <form className={styles.atualizar}>
+                    <div className={styles.input}>
+                        <TextInput 
+                            {...register("valor")}
+                            id={"valor"}
+                            label="Valor (R$)" 
+                            type="number"
+                            placeholder="R$ 0,00"
+                        />
+                        {errors.valor && <p className={styles.error} >{errors.valor.message}</p>}
+                    </div>
+
+                    <div className={styles.input}>
+                        <TextInput
+                            {...register("data")}
+                            id="data" 
+                            label="Data"
+                            type="date"
+                        />
+                        {errors.data && <p className={styles.error} >{errors.data.message}</p>}
+                    </div>
+        
+                    <div className={styles.input}>
+                        <TextInput 
+                            {...register("fonte")}
+                            id="fonte" 
+                            label="Fonte do Dinheiro (Ex: Sobra do Salário, Venda do Item 13)"
+                        />
+                        {errors.fonte && <p className={styles.error} >{errors.fonte.message}</p>}
+                    </div>
+
+                    <div className={styles.botoes}>
+                        <button className={styles.botao_atualizar}>Atualizar</button>
+                        <button className={styles.botao_cancelar}>Cancelar</button>
+                    </div>
+                </form>
             </Modal>
                             
             <table className={styles.tabela}>
@@ -78,7 +126,7 @@ export default function Historico() {
                         <td>R$ {reserva.valor}</td>
                         <td>{reserva.data}</td>
                         <td>
-                            <button><FaPen size={16}/></button>
+                            <button onClick={() => setEditModal(true)}><FaPen size={16}/></button>
                             <button onClick={() => abrirModal(reserva.id)}><FaTrash size={16}/></button>
                         </td>
                     </tr>
