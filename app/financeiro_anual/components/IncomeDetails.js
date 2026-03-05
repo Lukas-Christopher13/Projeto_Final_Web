b692e0a80981875f8007950b0522665c73038f2d
lukas_christopher <lukas.santana@aluno.uepb.edu.br>
2026-03-05 19:59:53 -0300

    front da parte 6 finalizado

--- app/financeiro_anual/components/IncomeDetails.js
+++ app/financeiro_anual/components/IncomeDetails.js
@@ -4,32 +4,34 @@ export default function IncomeDetails() {
     return (
         <div className={styles.income_details}>
             <h1>Detalhamento de Rendas no Ano</h1>
-            <table className={styles.table}>
-                <thead>
-                    <tr>
-                        <th>Data</th>
-                        <th>Descrição</th>
-                        <th>Valor</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>23/02/2023</td>
-                        <td>Salário</td>
-                        <td>R$ 1.400,00</td>
-                    </tr>
-                    <tr>
-                        <td>23/02/2023</td>
-                        <td>Salário</td>
-                        <td>R$ 1.400,00</td>
-                    </tr>
-                    <tr>
-                        <td>23/02/2023</td>
-                        <td>Salário</td>
-                        <td>R$ 1.400,00</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className={styles.table_container}>
+                <table className={styles.table}>
+                    <thead>
+                        <tr>
+                            <th>Data</th>
+                            <th>Descrição</th>
+                            <th>Valor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>23/02/2023</td>
+                            <td>Salário</td>
+                            <td>R$ 1.400,00</td>
+                        </tr>
+                        <tr>
+                            <td>23/02/2023</td>
+                            <td>Salário</td>
+                            <td>R$ 1.400,00</td>
+                        </tr>
+                        <tr>
+                            <td>23/02/2023</td>
+                            <td>Salário</td>
+                            <td>R$ 1.400,00</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
